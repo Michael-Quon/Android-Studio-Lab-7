64ed989697c0f4981237ef911dbb347e1e9f412d
Michael-Quon <michaelquon08@gmail.com>
2024-02-27 22:08:27 -0500

    Created the Michael fragment, added my name, and the radio group of the three buttons to have one selected by default and only one being selectable at a time. And an AlertDialog pops up with a picture and the label that is not dismissible without clicking "OK"

--- MichaelModule7/src/main/java/michael/quon/n01565129/MichaelFragment.java
+++ MichaelModule7/src/main/java/michael/quon/n01565129/MichaelFragment.java
@@ -1,6 +1,8 @@
 // Michael Quon N01565129
 package michael.quon.n01565129;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +10,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 
 public class MichaelFragment extends Fragment {
@@ -16,17 +20,40 @@ public class MichaelFragment extends Fragment {
         // Required empty public constructor
     }
 
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_michael, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_michael, container, false);
+
+        RadioGroup radioGroup = view.findViewById(R.id.MicRadioGroup);
+
+        RadioButton radioButton1 = view.findViewById(R.id.MicRadioButton1);
+        radioButton1.setChecked(true);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton selectedRadioButton = view.findViewById(checkedId);
+                displayAlertDialog(selectedRadioButton.getText().toString());
+            }
+        });
+        return view;
     }
+
+    private void displayAlertDialog(String radioButtonText) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setIcon(R.drawable.cn_tower);
+        builder.setTitle(getString(R.string.name));
+        builder.setMessage(radioButtonText);
+        builder.setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.setCancelable(false);
+        builder.show();
+    }
+
 }
